2bd94616618dd55b9841997ba9b335439fb2cb67
Holm der Kannenzwerg <hk@crylle.net>
2024-06-17 08:33:41 +0200

    --wip-- [skip ci]

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,1 @@
-alias less=cless
-alias cat=ccat
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -3,3 +3,6 @@ bindkey -e
 bindkey "^[[A" history-substring-search-up
 bindkey "^[[B" history-substring-search-down
 bindkey '^[w' kill-region
+
+bindkey '^[^[[1;9A' insert-last-word \
+        '^[b' insert-last-word
