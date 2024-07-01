158d2d5650c2463aa06b9b5679eb5b969d1ddfec
Holm der Kannenzwerg <hk@crylle.net>
2024-07-01 12:09:12 +0200

    alias mac-xca added

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias mac-xca="/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3307/QMYSQL:xca_ca --sqlpass=file:${HOME}/.xca/sql-pwd --password=file:${HOME}/.xca/pwd"
