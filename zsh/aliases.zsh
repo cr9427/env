339f32b9b582ae4ba60b50844a323de3689d96fd
Holm der Kannenzwerg <hk@crylle.net>
2024-07-02 09:08:27 +0200

    --wip-- [skip ci]

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,5 +7,5 @@ else
 fi
 
 if [ "$OS" = "arm-apple-darwin23.5.0" ]; then
-  alias mac-xca='/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3307/QMYSQL:xca_ca --sqlpass=file:${HOME}/.config/xca/sql-pwd --password=file:${HOME}/.config/xca/pwd'
+  alias xca='/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3307/QMYSQL:xca_ca --sqlpass=file:${HOME}/.config/xca/sql-pwd --password=file:${HOME}/.config/xca/pwd'
 fi
