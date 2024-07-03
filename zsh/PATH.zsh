bbecab7aa95f484ff0f6af46d3fa8987d6788ec6
Holm der Kannenzwerg <hk@crylle.net>
2024-07-03 09:29:14 +0200

    PATH customized

--- zsh/PATH.zsh
+++ zsh/PATH.zsh
@@ -1,6 +1,7 @@
 preList PATH /opt/homebrew/opt/coreutils/libexec/gnubin
+preList PATH /usr/local/opt/coreutils/libexec/gnubin
 preList PATH /opt/homebrew/opt/mysql-client/bin
-preList PATH /opt/homebrew/opt/libiodbc/bin
+preList PATH /usr/local/opt/mysql-client/bin
 preList PATH ${HOME}/run/${OS}/bin
 preList PATH ${HOME}/bin
 appList PATH /opt/puppetlabs/bin
