3572251f100a0fb42f0a2f998cf8773adc5ddb45
Holm der Kannenzwerg <hk@crylle.net>
2024-07-04 16:26:04 +0200

    --wip-- [skip ci]

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,9 +3,10 @@
 if [[ -f ${HOME}/.aliases ]]; then
   source ${HOME}/.aliases
 fi
+
 # handle eza w/ or w/o git
 #
-if [[ "$OS" =~ "aarch64-unknown-linux-gnu" ]]; then
+if [[ "${OS}" = "aarch64-unknown-linux-gnu" ]]; then
   alias ll="eza --color=always --long --header --icons=always --smart-group"
   alias la="eza --color=always --long --header --icons=always --smart-group --all"
 else
@@ -15,6 +16,6 @@ fi
 
 # alias for xca on mac
 #
-if [[ "$OS" = "arm-apple-darwin23.5.0" ]]; then
+if [[ "${OS}" = "arm-apple-darwin23.5.0" ]]; then
   alias xca="/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3307/QMYSQL:xca_ca --sqlpass=file:${HOME}/.config/xca/sql-pwd --password=file:${HOME}/.config/xca/pwd"
 fi
