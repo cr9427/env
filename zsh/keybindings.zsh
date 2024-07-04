d78064a4517892a19067b721ad74d18c5ea44559
Holm der Kannenzwerg <hk@crylle.net>
2024-07-04 14:20:59 +0200

    --wip-- [skip ci]

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,19 +1,19 @@
 # Keybindings
+
+function _history_substring_search_config() {
+  if [[ "$OS" =~ "darwin" ]]; then
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
+  elif [[ "$OS" =~ "linux" ]]; then
+      # https://superuser.com/a/1296543
+      # key dict is defined in /etc/zsh/zshrc
+      bindkey "$key[Up]" history-substring-search-up
+      bindkey "$key[Down]" history-substring-search-down
+  fi
+}
+
 bindkey -e
-bindkey "^[[A" history-substring-search-up
-bindkey "^[[B" history-substring-search-down
 bindkey '^[w' kill-region
 
 bindkey '^[^[[1;9A' insert-last-word \
         '^[b' insert-last-word
-
-# OS=$(uname -s)
-# if [ "$OS" = "Darwin" ]; then
-#     bindkey '^[[A' history-substring-search-up
-#     bindkey '^[[B' history-substring-search-down
-# elif [ "$OS" = "Linux" ]; then
-#     # https://superuser.com/a/1296543
-#     # key dict is defined in /etc/zsh/zshrc
-#     bindkey "$key[Up]" history-substring-search-up
-#     bindkey "$key[Down]" history-substring-search-down
-# fi
