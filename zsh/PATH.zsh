50e5a9e1a66d2cae26131a1e90e48eba540683d1
Holm der Kannenzwerg <hk@crylle.net>
2024-07-06 10:36:44 +0200

    --wip-- [skip ci]

--- zsh/PATH.zsh
+++ zsh/PATH.zsh
@@ -2,6 +2,8 @@ preList PATH /opt/homebrew/opt/coreutils/libexec/gnubin
 preList PATH /usr/local/opt/coreutils/libexec/gnubin
 preList PATH /opt/homebrew/opt/mysql-client/bin
 preList PATH /usr/local/opt/mysql-client/bin
+preList PATH /homes/linuxbrew/.linuxbrew/bin
+preList PATH /home/linuxbrew/.linuxbrew/bin
 preList PATH ${HOME}/run/${OS}/bin
 preList PATH ${HOME}/bin
 appList PATH /opt/puppetlabs/bin
