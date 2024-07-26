c908576625ccdb7e72d589b052ade1e55acc9504
Holm der Kannenzwerg <hk@crylle.net>
2024-07-26 13:01:22 +0200

    --wip-- [skip ci]

--- zsh/xca.zsh
+++ zsh/xca.zsh
@@ -1,9 +1,12 @@
 # alias for xca on mac
 #
 if [[ "${OS}" = "arm-apple-darwin23.5.0" ]]; then
-  alias xca="/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3307/QMYSQL:xca_ca --sqlpass=file:${HOME}/.config/xca/sql-pwd --password=file:${HOME}/.config/xca/pwd"
+  alias xca="/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3306/QMYSQL:xca_prod --sqlpass=file:${HOME}/.config/xca/sql-pwd --password=file:${HOME}/.config/xca/pwd"
+#  alias xca="/Applications/xca.app/Contents/MacOS/xca  --database=xca@sql.crylle.local:3306/QODBC:xca_prod --sqlpass=file:${HOME}/.config/xca/sql-pwd --password=file:${HOME}/.config/xca/pwd"
 fi
 
 # see https://man.uex.se/1/xca
 #
-export QMYSQL_OPTIONS="requiressl=1"
+#export QMYSQL_OPTIONS="requiressl=1"
+#export QODBC_OPTIONS="requiressl=1"
+#export DYLD_LIBRARY_PATH=/opt/homebrew/lib
