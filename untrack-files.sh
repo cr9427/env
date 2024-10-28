547db88fa12d67169fb34d42a9764d4c50fe60d2
Holm der Kannenzwerg <hk@crylle.net>
2024-10-28 12:59:33 +0100

    --wip-- [skip ci]

--- untrack-files.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-git update-index --skip-worktree stow/mc/.config/mc/ini
-git update-index --skip-worktree stow/ssh/.ssh/authorized_keys
-git update-index --skip-worktree stow/ssh/.ssh/config
-git update-index --skip-worktree stow/htop/.config/htop/htoprc
