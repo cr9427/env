28f705a7e8d3b7ca57c0355679fa66b0244b825a
Holm der Kannenzwerg <hk@crylle.net>
2024-11-09 15:48:27 +0100

    --wip-- [skip ci]

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -1,14 +1,14 @@
 # Set the directory we want to store zinit and plugins
 #
-export ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+export ZINIT[BIN_DIR]=${HOME}/run/share/zinit/zinit.git
 
 # Download Zinit, if it's not there yet
 #
-if [ ! -d "$ZINIT_HOME" ]; then
-   mkdir -p "$(dirname $ZINIT_HOME)"
-   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+if [ ! -d "$ZINIT_BIN" ]; then
+   mkdir -p "$(dirname $ZINIT_BIN)"
+   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_BIN"
 fi
 
 # Source/Load zinit
 #
-source "${ZINIT_HOME}/zinit.zsh"
+source "${ZINIT_BIN}/zinit.zsh"
