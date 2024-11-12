094b5f983fa797ea184f2db9158513d2dced89db
Holm der Kannenzwerg <hk@crylle.net>
2024-11-12 07:37:18 +0100

    untrach-files.sh and k9s for orbstack added

---
+++ untrack-files.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+git update-index --skip-worktree stow/mc/.config/mc/ini
+git update-index --skip-worktree stow/ssh/.ssh/authorized_keys
+git update-index --skip-worktree stow/ssh/.ssh/config
+git update-index --skip-worktree stow/htop/.config/htop/htoprc
+git update-index --skip-worktree stow/step-cli/dot-step/config/defaults.json
