4fdf08b145af8686921839648dee67fe655ce21a
Holm der Kannenzwerg <hk@crylle.net>
2025-01-16 11:42:10 +0100

    you-should-use execptions for xca added

--- zsh/you-should-use.zsh
+++ zsh/you-should-use.zsh
@@ -1,5 +1,5 @@
 export YSU_MESSAGE_POSITION="after"
 # export YSU_MODE=BESTMATCH
 export YSU_MODE=ALL
-export YSU_IGNORED_ALIASES=("g" "ll" "k")
+export YSU_IGNORED_ALIASES=("g" "ll" "k" "xca")
 export YSU_HARDCORE=1
