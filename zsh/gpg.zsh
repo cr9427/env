399485444eabd7a7403b0fb715b26dbba5d80ec3
Holm der Kannenzwerg <hk@crylle.net>
2025-02-01 14:48:39 +0100

    gpg-agent preset passphrase with 1 password

---
+++ zsh/gpg.zsh
@@ -0,0 +1,2 @@
+export GPG_TTY=$(tty)
+op item get wxho7pyc4vjxcf2vtq6uf6mvdi --fields GPG-Passphrase --reveal | "$(gpgconf --list-dirs libexecdir)"/gpg-preset-passphrase --preset AF4D9B2D2069C69444A7C43CD67C6AD8F47FB63E
