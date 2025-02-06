3905f654bb3869448c8d467d2cfdb9783003126a
Holm der Kannenzwerg <hk@crylle.net>
2025-02-06 17:45:48 +0100

    gpg agent triggered at login

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -1,2 +1,3 @@
 export GPG_TTY=$(tty)
+gpg-connect-agent /bye &> /dev/null
 op item get wxho7pyc4vjxcf2vtq6uf6mvdi --fields GPG-Passphrase --reveal | "$(gpgconf --list-dirs libexecdir)"/gpg-preset-passphrase --preset AF4D9B2D2069C69444A7C43CD67C6AD8F47FB63E
