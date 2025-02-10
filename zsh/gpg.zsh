b91513e2c1334b5e602ed05de0333f0a369530b7
Holm der Kannenzwerg <hk@crylle.net>
2025-02-10 10:47:25 +0100

    gpg settings customized

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -1,3 +1,5 @@
 export GPG_TTY=$(tty)
 gpg-connect-agent /bye &> /dev/null
 op item get wxho7pyc4vjxcf2vtq6uf6mvdi --fields GPG-Passphrase --reveal | "$(gpgconf --list-dirs libexecdir)"/gpg-preset-passphrase --preset AF4D9B2D2069C69444A7C43CD67C6AD8F47FB63E
+
+alias gpg-load-key='op item get wxho7pyc4vjxcf2vtq6uf6mvdi --fields GPG-Passphrase --reveal | "$(gpgconf --list-dirs libexecdir)"/gpg-preset-passphrase --preset AF4D9B2D2069C69444A7C43CD67C6AD8F47FB63E'
