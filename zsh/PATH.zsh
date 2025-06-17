b5fe7409dee5fbf107557d901244c03091617312
Holm der Kannenzwerg <hk@crylle.net>
2025-06-17 11:09:43 +0200

    path for puppet development kit (pdk) added

--- zsh/PATH.zsh
+++ zsh/PATH.zsh
@@ -9,6 +9,7 @@ appList PATH /usr/local/sbin
 appList PATH /usr/sbin
 appList PATH /sbin
 appList PATH /opt/puppetlabs/bin
+appList PATH /opt/puppetlabs/pdk/bin
 
 export  DYLD_LIBRARY_PATH="-L"
 appList DYLD_LIBRARY_PATH /opt/homebrew/opt/mariadb-connector-c/lib
