2709b924704e0293a7b8b57afe486dc21ef2ae09
Holm der Kannenzwerg <hk@crylle.net>
2025-10-19 14:44:43 +0200

    housekeeping

--- untrack-files.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-git update-index --skip-worktree stow/mc/.config/mc/ini
-git update-index --skip-worktree stow/ssh/.ssh/authorized_keys
-git update-index --skip-worktree stow/ssh/.ssh/config
-git update-index --skip-worktree stow/htop/.config/htop/htoprc
-git update-index --skip-worktree stow/step-cli/dot-step/config/defaults.json
